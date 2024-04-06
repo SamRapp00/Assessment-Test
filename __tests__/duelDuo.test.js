b240ff366e332b8df2c9940c84a87883cc2ca872
Sam Christen <samchristenlds@gmail.com>
2024-04-06 11:21:34 -0600

    second commit

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -16,26 +16,39 @@ describe("Duel Duo tests", () => {
     await driver.wait(until.titleIs("Duel Duo"), 1000);
   });
 
-  test("clicking the Draw button displays the div with id = 'choices'", async () => {
+  test("clicking the Draw button displays the div with id choices", async () => {
     await driver.get("http://localhost:8000");
-    const drawButton = await driver.findElement(By.id("draw-button"));
-    await drawButton.click();
-    await driver.wait(until.elementLocated(By.id("choices")), 1000);
-  });
+    const drawBtn = await driver.findElement(By.id("draw"));
+    await drawBtn.click();
+    const choicesDiv = await driver.findElement(By.id("choices"));
+    const display = await choicesDiv.isDisplayed();
+    expect(display).toBe(true);
+  })
 
   test("clicking an 'Add to Duo' button displays the div with id = 'player-duo'", async () => {
     await driver.get("http://localhost:8000");
-    const addButton = await driver.findElement(By.className("add-to-duo-button"));
+    const addButton = await driver.findElement(By.className("bot-btn"));
     await addButton.click();
     await driver.wait(until.elementLocated(By.id("player-duo")), 1000);
   });
+  //Not Sure If this test will run^^^
 
-  test("when a bot is 'Removed from Duo', it goes back to 'choices'", async () => {
+  test("remove bot from duo returns it to choices", async () => {
     await driver.get("http://localhost:8000");
-    const addButton = await driver.findElement(By.className("add-to-duo-button"));
-    await addButton.click();
-    const removeButton = await driver.findElement(By.className("remove-from-duo-button"));
-    await removeButton.click();
-    await driver.wait(until.elementLocated(By.id("choices")), 1000);
+    const drawButton = await driver.findElement(By.id("draw"));
+    await drawButton.click();
+    const addToDuoButtons = await driver.findElements(By.className("bot-btn"));
+    await addToDuoButtons[0].click();
+    const playerDuoDiv = await driver.findElement(By.id("player-duo"));
+    const botInPlayerDuo = await playerDuoDiv.findElement(
+      By.className("bot-card")
+    );
+    expect(await botInPlayerDuo.isDisplayed()).toBe(true);
+    const removeFromDuoButtons = await driver.findElements(
+      By.className("bot-btn")
+    );
+    await removeFromDuoButtons[0].click();
+    const choicesDiv = await driver.findElement(By.id("choices"));
+    const botInChoices = await choicesDiv.findElement(By.className("bot-card"));
+    expect(await botInChoices.isDisplayed()).toBe(true);
   });
-});
